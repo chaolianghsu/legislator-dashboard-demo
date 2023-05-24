1ea2a39dc2bee11942f2bbcf344a80bcd310e47d
Eddy <milkeddy1@dailyview.tw>
2023-05-24 11:13:36 +0800

    feat: mock 選區競爭 api

--- src/mocks/server.js
+++ src/mocks/server.js
@@ -14,6 +14,7 @@ import {
   xlsxAPIs,
   swingModuleAPIs,
   competitorAPIs,
+  constituencyAPIs,
   partyAdvantageAPIs,
 } from './handlers'
 
@@ -33,5 +34,6 @@ export const server = setupServer(
   ...xlsxAPIs,
   ...swingModuleAPIs,
   ...competitorAPIs,
+  ...constituencyAPIs,
   ...partyAdvantageAPIs,
 )
