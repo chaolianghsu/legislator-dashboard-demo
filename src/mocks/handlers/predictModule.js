c6e3a7be816e88335af8cf83c71410a8105be219
Eddy <milkeddy1@dailyview.tw>
2023-05-25 18:13:16 +0800

    refactor: predict module mock data

--- src/mocks/handlers/predictModule.js
+++ src/mocks/handlers/predictModule.js
@@ -7,44 +7,55 @@ const predictModuleAPIs = [
   rest.get(Url, (req, res, ctx) => res(
     ctx.delay(500),
     ctx.status(200),
-    ctx.json({
-      result: [
-        {
-          date: '2023/04/16 ~ 2023/05/15',
-          update: '2023/05/21',
-          person: [
-            {
-              member_id: 1,
-              person_id: 0,
-              name: '羅智強',
-              image: 'https://stationhousevets.com/wp-content/uploads/2021/10/closeup-shot-guinea-pig-isolated-scaled.jpg',
-              party: '中國國民黨',
-              is_main: true,
-              election_success_rate: 47.68,
+    ctx.json(
+      {
+        result: [
+          {
+            date: '2023/04/24 ~ 2023/05/24',
+            update: '2023/05/24',
+            person: [
+              {
+                member_id: 1,
+                person_id: 1,
+                name: '徐巧芯',
+                image: null,
+                party: '國民黨',
+                is_main: true,
+                election_success_rate: null,
+              },
+              {
+                member_id: 1,
+                person_id: 2,
+                name: '許淑華',
+                image: null,
+                party: '民進黨',
+                is_main: false,
+                election_success_rate: null,
+              },
+            ],
+            party_advantage: 0,
+            swing_rate: 0,
+            vol: {
+              total: 177958,
+              grow: '-23.78',
             },
-          ],
-          party_advantage: 29614,
-          swing_rate: null,
-          vol: {
-            total: 396,
-            grow: '-86.88',
+            reputation: {
+              total: 312525.5,
+              grow: -30.3,
+            },
+            favorability: {
+              total: 0.51,
+              grow: '-17.74',
+            },
+            social_rc: 0,
+            social_touch: 0,
+            diffusion: 86,
+            interaction: 31.4,
           },
-          reputation: {
-            total: 1667.4,
-            grow: -76,
-          },
-          favorability: {
-            total: 1.45,
-            grow: '339.39',
-          },
-          social_rc: 0,
-          social_touch: 0,
-          diffusion: 29,
-          interaction: 51.7,
-        },
-      ],
-      code: 0,
-    }),
+        ],
+        code: 0,
+      },
+    ),
   )),
 ]
 
