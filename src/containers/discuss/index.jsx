43ab10c6ffd25f91a6e0d487ac6d5dc1ccf9f635
Eddy <milkeddy1@dailyview.tw>
2023-05-26 15:53:16 +0800

    feat: apply 施政好感度 api

--- src/containers/discuss/index.jsx
+++ src/containers/discuss/index.jsx
@@ -1,11 +1,30 @@
 import { Box } from '@mui/material'
-import { BarLineChart, TitleData, Card } from '@/components'
+import { useQuery } from '@tanstack/react-query'
+import {
+  BarLineChart, TitleData, Card, LoadingProgress,
+} from '@/components'
+import { satisfactionAPI } from '@/apis'
 
 function DiscussContainer() {
+  const {
+    data, isFetching, isLoading, isError,
+  } = useQuery({
+    queryKey: [satisfactionAPI.Url],
+    queryFn: () => satisfactionAPI.getData(),
+    select: (d) => d.result[0],
+  })
+  if (isFetching || isLoading || isError) {
+    return <LoadingProgress />
+  }
+
+  const categories = data.data?.map((item) => item.name)
+  const lineData = data.data?.map((item) => item.satisfaction)
+  const barData = data.data?.map((item) => item.vol)
+
   return (
     <Box padding={1.5}>
-      <Card title={<TitleData title="政策討論度" />}>
-        <BarLineChart />
+      <Card title={<TitleData title="政策討論度" value={data.public_satisfaction} />}>
+        <BarLineChart categories={categories} lineData={lineData} barData={barData} />
       </Card>
     </Box>
   )
