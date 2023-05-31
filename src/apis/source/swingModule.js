2c882366f830fc13fe696a336cfb9d3334995f0f
Eddy <milkeddy1@dailyview.tw>
2023-05-31 17:27:15 +0800

    refactor: swing mock data and end point

--- src/apis/source/swingModule.js
+++ src/apis/source/swingModule.js
@@ -1,6 +1,6 @@
 import { axiosInstance } from '../axiosInstance'
 
-export const Url = '/main/constituency_swing_rate_module'
+export const Url = '/main/constituency_swing_rate'
 
 export const getData = async () => {
   const res = await axiosInstance.get(Url)
