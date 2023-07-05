1f5133e89d0980b205af36c91ca2cd06d8b675de
Eddy <milkeddy1@dailyview.tw>
2023-07-05 11:03:58 +0800

    fix: disable zustand localstorage function
    造成日期和預期不符合原因是被localstorage存起來了

--- src/store/useGlobalDateStore.js
+++ src/store/useGlobalDateStore.js
@@ -1,23 +1,20 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
 import { addDays } from 'date-fns'
 
 const useGlobalDateStore = create(
-  persist(
-    (set) => (
-      {
-        startDate: addDays(new Date(), -30),
-        endDate: addDays(new Date(), -1),
-        update: ({ startDate, endDate }) => set({
-          startDate,
-          endDate,
-        }),
-      }
-    ),
+  (set) => (
     {
-      name: 'globalDate',
-    },
+      startDate: addDays(new Date(), -30),
+      endDate: addDays(new Date(), -1),
+      update: ({ startDate, endDate }) => set({
+        startDate,
+        endDate,
+      }),
+    }
   ),
+  {
+    name: 'globalDate',
+  },
 )
 
 export default useGlobalDateStore
